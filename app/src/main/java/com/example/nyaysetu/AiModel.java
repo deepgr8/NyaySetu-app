ec2f745d03c936db1bcc6135a2e5369056c823c9
deepgr8 <deepuprajapti5@gmail.com>
2024-03-04 19:05:08 +0530

    third commit

---
+++ app/src/main/java/com/example/nyaysetu/AiModel.java
@@ -0,0 +1,36 @@
+package com.example.nyaysetu;
+
+import androidx.core.content.ContextCompat;
+
+import com.google.ai.client.generativeai.GenerativeModel;
+import com.google.ai.client.generativeai.java.GenerativeModelFutures;
+import com.google.ai.client.generativeai.type.Content;
+import com.google.ai.client.generativeai.type.GenerateContentResponse;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
+
+
+public class AiModel {
+
+    private static final String apiKey = "AIzaSyBanBCnl7DjDbZ8MeSFN9rv290bEZ1qMSM";
+
+    static String result(String userQuery){
+        GenerativeModel gm  = new GenerativeModel("gemini-pro",apiKey);
+        GenerativeModelFutures modelFutures = GenerativeModelFutures.from(gm);
+        Content content = new Content.Builder()
+                .addText(userQuery)
+                .build();
+        ListenableFuture<GenerateContentResponse> response = modelFutures.generateContent(content);
+        final String[] promptResult = {""};
+        response.addListener(()->{
+            try {
+                GenerateContentResponse response1 = response.get();
+                promptResult[0] += response1.getText();
+            } catch (ExecutionException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, ContextCompat.getMainExecutor(null));
+        return promptResult[0];
+    }
+}
--- app/src/main/java/com/example/nyaysetu/MainActivity.java
+++ app/src/main/java/com/example/nyaysetu/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.nyaysetu;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -8,7 +10,9 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Application;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 
@@ -16,12 +20,20 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.color.DynamicColors;
 import com.google.android.material.color.utilities.DynamicColor;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private static final int SPEECH_REQUEST_CODE = 0;
+    String spokenText="";
+    String generated_response="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        speechRecognizer();
+        QueryPreprocessing queryPreprocessing = new QueryPreprocessing();
+        queryPreprocessing.checkTerm(getApplicationContext());
 //        apply to all activities and fragments.....remainder
         DynamicColors.applyToActivitiesIfAvailable((Application) getApplicationContext());
 
@@ -37,14 +49,34 @@ public class MainActivity extends AppCompatActivity {
                 } else  {
                     loadFragment(new CategoryFragment() , false);
                 }
-
                 return true;
             }
         });
 
     }
 
-   private void loadFragment(Fragment fragment , boolean isAppInitialized){
+    private void speechRecognizer() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        startActivityForResult(intent,SPEECH_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==SPEECH_REQUEST_CODE && resultCode==RESULT_OK){
+            List<String> result  = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            if (result!=null && !result.isEmpty()){
+                spokenText = result.get(0);
+//                setting user said query on text box
+                if(QueryPreprocessing.search(spokenText)){
+                    generated_response=AiModel.result(spokenText);
+                }
+            }
+        }
+    }
+
+    private void loadFragment(Fragment fragment , boolean isAppInitialized){
        FragmentManager fragmentManager = getSupportFragmentManager();
        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
        if (isAppInitialized){
