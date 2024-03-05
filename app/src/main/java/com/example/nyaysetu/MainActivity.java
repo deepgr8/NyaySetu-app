09f079f0d750989d112c8bf39f6d8a5695c4f036
deepgr8 <deepuprajapti5@gmail.com>
2024-03-05 19:19:59 +0530

    third commit dd

--- app/src/main/java/com/example/nyaysetu/MainActivity.java
+++ app/src/main/java/com/example/nyaysetu/MainActivity.java
@@ -1,24 +1,19 @@
 package com.example.nyaysetu;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.annotation.SuppressLint;
 import android.app.Application;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.color.DynamicColors;
-import com.google.android.material.color.utilities.DynamicColor;
-
 import java.util.ArrayList;
-import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
     private AdvocateAdapter advocateAdapter;
@@ -36,12 +31,12 @@ public class MainActivity extends AppCompatActivity {
        FrameLayout frameLayout =findViewById(R.id.frameLayout);
 //     .....................................................................
         recyclerView = findViewById(R.id.recyclerView);
-        List<AdvocateModel> list = new ArrayList<>();
+        ArrayList<AdvocateModel> list = new ArrayList<>();
         list.add(new AdvocateModel("swati" , 5));
         list.add(new AdvocateModel("damini" , 5));
         list.add(new AdvocateModel("deepu" , 5));
         list.add(new AdvocateModel("aryan" , 5));
-        AdvocateAdapter advocateAdapter1 = new AdvocateAdapter((ArrayList<AdvocateModel>) list, getApplicationContext());
+        AdvocateAdapter advocateAdapter1 = new AdvocateAdapter(list, getApplicationContext());
         recyclerView.setAdapter(advocateAdapter1);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
